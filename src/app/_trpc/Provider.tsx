527d3a117d38db2ef8e1d7f4e769584e74aafde2
Michael Bryce <michaelbryce@me.com>
2024-04-01 22:24:04 -0600

    fix: provider

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import React, { useState } from "react";
 
 import { trpc } from "./client";
@@ -20,10 +20,19 @@ function getBaseUrl() {
 }
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({}));
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { staleTime: 5 * 1000 } },
+      })
+  );
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
+        loggerLink({
+          enabled: () => true,
+        }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
         }),
